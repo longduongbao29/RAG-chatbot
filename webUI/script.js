3e39e44f02f989611ffa7547133154c8d5bb0834
longdb <longduongbao29@gmail.com>
2025-04-06 12:21:07 +0000

    [fix] Certificate HTTPS

--- webUI/script.js
+++ webUI/script.js
@@ -14,7 +14,7 @@ const fileInput = document.getElementById('fileInput');
 const fileNameSpan = document.getElementById('fileName');
 const uploadBtn = document.getElementById('uploadBtn');
 
-API_URL = "http://52.194.241.126:8002/api"
+API_URL = "https://52.194.241.126:8002/api"
 
 
 // Hàm thêm tin nhắn vào khung chat
