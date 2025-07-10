4880bc501ee1bc4f196b44bf60d113c8d6749701
longdb <longduongbao29@gmail.com>
2025-07-11 02:52:12 +0700

    [gitflow] init

--- test_rag/test.py
+++
@@ -1,3 +0,0 @@
-from src.rag.strategy.indexing.chunking.Chunker import Chunker
-
-print(Chunker.default_chunk_text("abc"))
--- test_rag/test_chunking.py
+++
@@ -1,5 +0,0 @@
-from src.rag.strategy.chunking.loader.PDFLoader import PDFLoader
-
-loader = PDFLoader("test_rag/Final_QC-dH-_2014_Ban-hanh-25-12-2014.pdf")
-
-print(loader.chunk_text())
